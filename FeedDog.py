cf3c246cf2c39b5a8c7c372747ece4f6e562800a
drewleean1 <drewleean@gmail.com>
2023-02-10 11:18:13 -0700

    second attempt with sorted arrays

--- FeedDog.py
+++ FeedDog.py
@@ -5,21 +5,23 @@
 
 
 def feedDog(hunger_level, biscuit_size):
-    #merge sort both arrays
+    #sort both arrays
     #starting from the largest biscuit try to find a corresponding hunger level, using a binary search?
     #if not give to the next largest dog
+    hunger_level.sort()
+    biscuit_size.sort()
+    dogs_left = len(hunger_level) - 1
+    biscuits_left = len(biscuit_size) - 1
     number_fed = 0
-    for x in hunger_level:
-        if x in biscuit_size:
-            hunger_level.remove(x)
-            biscuit_size.remove(x)
+
+    while dogs_left >=0 and biscuits_left >= 0:
+        if biscuit_size[biscuits_left] >= hunger_level[dogs_left]:
+            dogs_left -= 1
+            biscuits_left -= 1
             number_fed += 1
-    for x in hunger_level:
-        for y in biscuit_size:
-            if x <= y:
-                hunger_level.remove(x)
-                biscuit_size.remove(y)
-                number_fed += 1
+        else:
+            dogs_left -= 1
+
     return number_fed
 
-
+#print(feedDog([1,5], [1,2,3]))
